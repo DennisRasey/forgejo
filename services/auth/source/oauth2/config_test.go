DennisRasey/forgejo

commit f538db4755477e28500520f1842ddf6e4bf2f3ed

fix: Initialize oauth2 only if enabled (#13483)
Fixes #13478

Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/13483
Reviewed-by: Andreas Ahlenstorf <aahlenst@noreply.codeberg.org>
Reviewed-by: Gusted <gusted@noreply.codeberg.org>

services/auth/source/oauth2/init.go
@@ -38,6 +38,9 @@ var DefaultVerifier *jwtx.Verifier
 // Init initializes the oauth source
 func Init(ctx context.Context) error {
 	var err error
+	if !setting.OAuth2.Enabled {
+		return nil
+	}
 	DefaultSigningKey, DefaultVerifier, err = jwtx.Init(&setting.OAuth2.KeyCfg)
 	if err != nil {
 		return err

services/auth/source/oauth2/config_test.go
@@ -1,0 +1,26 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package oauth2_test
+
+import (
+	"testing"
+
+	"forgejo.org/modules/setting"
+	"forgejo.org/modules/test"
+	"forgejo.org/services/auth/source/oauth2"
+
+	"github.com/stretchr/testify/require"
+)
+
+// regression #13478
+func TestOAuth2Disabled(t *testing.T) {
+	cfg, _ := setting.NewConfigProviderFromData(`
+[oauth2]
+ENABLED=false
+`)
+	defer test.MockVariableValue(&setting.CfgProvider, cfg)()
+	setting.LoadCommonSettings()
+	err := oauth2.Init(t.Context())
+	require.NoError(t, err)
+}

commit 10beaf54ece70c1bfcd98b0655a25ca343b0d3f3

fix oauth2: Initialize gothic even if oauth2 is disabled (#13584)
Regression from #13483

Fixes #13561
Fixes #13563

Related to https://codeberg.org/forgejo/forgejo/issues/13541#issuecomment-19756790

Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/13584
Reviewed-by: Mathieu Fenniak <mfenniak@noreply.codeberg.org>

services/auth/source/oauth2/init.go
@@ -38,13 +38,6 @@ var DefaultVerifier *jwtx.Verifier
 // Init initializes the oauth source
 func Init(ctx context.Context) error {
 	var err error
-	if !setting.OAuth2.Enabled {
-		return nil
-	}
-	DefaultSigningKey, DefaultVerifier, err = jwtx.Init(&setting.OAuth2.KeyCfg)
-	if err != nil {
-		return err
-	}
 
 	// Lock our mutex
 	gothRWMutex.Lock()
@@ -65,6 +58,14 @@ func Init(ctx context.Context) error {
 
 	// Unlock our mutex
 	gothRWMutex.Unlock()
+
+	if !setting.OAuth2.Enabled {
+		return nil
+	}
+	DefaultSigningKey, DefaultVerifier, err = jwtx.Init(&setting.OAuth2.KeyCfg)
+	if err != nil {
+		return err
+	}
 
 	return initOAuth2Sources(ctx)
 }

services/auth/source/oauth2/config_test.go
@@ -10,10 +10,11 @@ import (
 	"forgejo.org/modules/test"
 	"forgejo.org/services/auth/source/oauth2"
 
+	"github.com/markbates/goth/gothic"
 	"github.com/stretchr/testify/require"
 )
 
-// regression #13478
+// regressions: Components used even if OAuth2 disabled
 func TestOAuth2Disabled(t *testing.T) {
 	cfg, _ := setting.NewConfigProviderFromData(`
 [oauth2]
@@ -22,5 +23,9 @@ ENABLED=false
 	defer test.MockVariableValue(&setting.CfgProvider, cfg)()
 	setting.LoadCommonSettings()
 	err := oauth2.Init(t.Context())
+	// #13478
 	require.NoError(t, err)
+
+	// #13561 #13563
+	require.IsType(t, &oauth2.SessionsStore{}, gothic.Store)
 }