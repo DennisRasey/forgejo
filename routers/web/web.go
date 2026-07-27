DennisRasey/forgejo

commit cdd35458a94873115305f43a592d2291b258c711

feat: enable auth to git LFS via authorized integrations (#12725)
The goal is to enable access to Git LFS resources with Authorized Integrations JWTs.

Blocker that needed to be resolved is that adding the `AuthorizedIntegration` auth method would conflict with the LFS tokens, which are handed out during git ssh clones to allow access to LFS resources -- `AuthorizedIntegration` would mark these as `AuthenticationAttemptedIncorrectCredential`, and therefore the requests would 401 before they got to the LFS-specific token validation routines.  The fix is to move LFS token authentication into an authentication group so that it could be resolved at the same time as the authorized integration, rather than doing it inside the LFS server routines.

Refactors for LFS tokens are covered by refreshed test automation.  Authorized integrations LFS Access has been manually tested, and will be further covered in an end-to-end integration test (https://code.forgejo.org/forgejo/end-to-end/pulls/1954).

## Checklist

The [contributor guide](https://forgejo.org/docs/next/contributor/) contains information that will be helpful to first time contributors. All work and communication must conform to Forgejo's [AI Agreement](https://codeberg.org/forgejo/governance/src/branch/main/AIAgreement.md). There also are a few [conditions for merging Pull Requests in Forgejo repositories](https://codeberg.org/forgejo/governance/src/branch/main/PullRequestsAgreement.md). You are also welcome to join the [Forgejo development chatroom](https://matrix.to/#/#forgejo-development:matrix.org).

### Tests for Go changes

- I added test coverage for Go changes...
  - [x] in their respective `*_test.go` for unit tests.
  - [ ] in the `tests/integration` directory if it involves interactions with a live Forgejo server.
- I ran...
  - [ ] `make pr-go` before pushing

### Documentation

routers/web/web.go
@@ -105,11 +105,15 @@ func optionsCorsHandler() func(next http.Handler) http.Handler {
 // earlier authentication success would prevent later authentication methods from being attempted.
 func buildAuthGroup() *auth_method.Group {
 	group := auth_method.NewGroup()
-	group.Add(&auth_method.OAuth2{}) // FIXME: this should be removed and only applied in download and oauth related routers
-	group.Add(&auth_method.Basic{})  // FIXME: this should be removed and only applied in download and git/lfs routers
 
-	// FIXME: extracted from OAuth2 & Basic -- these methods have internal URL filters that should be moved into
-	// middlewares (if we can figure out the right way to do that), similar to the notes on OAuth2 & Basic above.
+	// FIXME: OAuth2, Basic, AccessToken, ActionRuntimeToken, ActionTaskToken authentication methods all use path
+	// matching so that they only authenticate requests in specific endpoints -- in general these auth methods aren't
+	// permitted for web endpoints, and aren't supported because the security models don't match.  (For example, they
+	// can have scoped permissions, which web UI handlers don't implement.) A clearer way to do this would be to apply
+	// specific authentication methods to their auth middlewares.  buildGitLfsAuthGroup and buildGitAuthGroup are the
+	// start of transitioning to that model.  Eventually these methods will be fully removed from buildAuthGroup:
+	group.Add(&auth_method.OAuth2{})
+	group.Add(&auth_method.Basic{})
 	group.Add(&auth_method.AccessToken{})
 	group.Add(&auth_method.ActionRuntimeToken{})
 	group.Add(&auth_method.ActionTaskToken{})
@@ -122,8 +126,24 @@ func buildAuthGroup() *auth_method.Group {
 	return group
 }
 
-// Authentication methods that are applied to Git HTTP & Git LFS HTTP routes.  They are processed in the order defined;
-// an earlier authentication success would prevent later authentication methods from being attempted.
+// Authentication methods that are applied to Git LFS HTTP routes.  They are processed in the order defined; an earlier
+// authentication success would prevent later authentication methods from being attempted.
+func buildGitLfsAuthGroup() *auth_method.Group {
+	group := auth_method.NewGroup()
+	group.Add(&auth_method.LFSToken{})
+	group.Add(&auth_method.Basic{})
+	group.Add(&auth_method.AccessToken{})
+	group.Add(&auth_method.ActionTaskToken{})
+	group.Add(&auth_method.AuthorizedIntegration{
+		// "Authorization: Basic ..." is easier to use for git operations, and already supported for other tokens, so it
+		// is enabled for Authorized Integrations as well:
+		PermitBasic: true,
+	})
+	return group
+}
+
+// Authentication methods that are applied to Git HTTP routes.  They are processed in the order defined; an earlier
+// authentication success would prevent later authentication methods from being attempted.
 func buildGitAuthGroup() *auth_method.Group {
 	group := auth_method.NewGroup()
 	group.Add(&auth_method.OAuth2{})
@@ -340,24 +360,10 @@ func Routes() *web.Route {
 		// TODO: GetNotificationCount & GetActiveStopwatch really seem like things that could be folded into Contexter or as helper functions
 		user.GetNotificationCount, repo.GetActiveStopwatch,
 		goGet)
-
-	// /{username}/{repo}/info/lfs routes currently need to use buildAuthGroup(), not buildGitAuthGroup(), because:
-	//
-	// a) there are tests that use session auth to access the LFS endpoints (TestAPILFSLocksLogged), which may be a test
-	// error, and
-	//
-	// b) if AuthorizedIntegration is included then JWTs generated by the LFS system with `setting.LFS.SigningKey` will
-	// return AuthenticationAttemptedIncorrectCredential from AuthorizedIntegration, and cause the requests to 401
-	// before reaching the LFS handlers.
-	//
-	// (a) is probably an error that can be fixed, and (b) should be fixed by changing LFS's JWT handling to be an
-	// `auth_service.Method` implementation which would be incorporated into the auth group, so that LFS isn't doing
-	// it's own "after the authentication" authentication.  In the interm, it's split out from the `registerRoutes` call
-	// above because at least fewer middlewares can be safely applied.
 	routes.Group("",
 		func() {
 			registerGitLFSRoutes(routes)
-		}, gzipMid, common.Sessioner(), context.Contexter(), webAuth(buildAuthGroup()), goGet)
+		}, gzipMid, common.Sessioner(), context.Contexter(), webAuth(buildGitLfsAuthGroup()), goGet)
 	routes.Group("",
 		func() {
 			registerGitRoutes(routes)

cmd/serv.go
@@ -23,13 +23,13 @@ import (
 	"forgejo.org/models/perm"
 	"forgejo.org/modules/git"
 	"forgejo.org/modules/json"
+	"forgejo.org/modules/lfs"
 	"forgejo.org/modules/log"
 	"forgejo.org/modules/pprof"
 	"forgejo.org/modules/private"
 	"forgejo.org/modules/process"
 	repo_module "forgejo.org/modules/repository"
 	"forgejo.org/modules/setting"
-	"forgejo.org/services/lfs"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/kballard/go-shellquote"

modules/lfs/shared.go
@@ -9,6 +9,8 @@ import (
 	"time"
 
 	"forgejo.org/modules/util"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 const (
@@ -116,4 +118,12 @@ type ErrorResponse struct {
 	Message          string
 	DocumentationURL string `json:"documentation_url,omitempty"`
 	RequestID        string `json:"request_id,omitempty"`
+}
+
+// Claims is a JWT Token Claims
+type Claims struct {
+	RepoID int64
+	Op     string
+	UserID int64
+	jwt.RegisteredClaims
 }

services/auth/method/auth_result_lfs_token.go
@@ -1,0 +1,42 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package method
+
+import (
+	auth_model "forgejo.org/models/auth"
+	user_model "forgejo.org/models/user"
+	"forgejo.org/modules/lfs"
+	"forgejo.org/modules/optional"
+	"forgejo.org/services/auth"
+	"forgejo.org/services/authz"
+)
+
+var _ auth.AuthenticationResult = &lfsTokenAuthenticationResult{}
+
+type lfsTokenAuthenticationResult struct {
+	*auth.BaseAuthenticationResult
+	user   *user_model.User
+	claims *lfs.Claims
+}
+
+func (r *lfsTokenAuthenticationResult) User() *user_model.User {
+	return r.user
+}
+
+func (r *lfsTokenAuthenticationResult) Scope() optional.Option[auth_model.AccessTokenScope] {
+	if r.claims.Op == "download" {
+		return optional.Some(auth_model.AccessTokenScopeReadRepository)
+	}
+	return optional.Some(auth_model.AccessTokenScopeWriteRepository)
+}
+
+func (r *lfsTokenAuthenticationResult) Reducer() authz.AuthorizationReducer {
+	return &authz.SpecificReposAuthorizationReducer{
+		ResourceRepos: []authz.RepoGetter{r},
+	}
+}
+
+func (r *lfsTokenAuthenticationResult) GetTargetRepoID() int64 {
+	return r.claims.RepoID
+}

services/auth/method/lfs_token.go
@@ -1,0 +1,66 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package method
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	user_model "forgejo.org/models/user"
+	"forgejo.org/modules/lfs"
+	"forgejo.org/modules/setting"
+	"forgejo.org/services/auth"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var _ auth.Method = &LFSToken{}
+
+// LFSToken is an authentication method used when access a git repository over ssh which has LFS resources in-use.  The
+// LFS client will issue a `git-lfs-authenticate` command over the ssh connection, and Forgejo will provide a
+// supplemental HTTP header "Authorization: Bearer ..." with a JWT.  The LFS client can then make HTTP requests to LFS
+// endpoints with that supplemental header in order to inherit the permissions of the SSH user and to retrieve LFS
+// objects.
+type LFSToken struct{}
+
+func (a *LFSToken) Verify(req *http.Request, w http.ResponseWriter, _ auth.SessionStore) auth.MethodOutput {
+	hasToken, tokenText := tokenFromAuthorizationBearer(req).Get()
+	if !hasToken {
+		return &auth.AuthenticationNotAttempted{}
+	}
+
+	token, err := jwt.ParseWithClaims(tokenText, &lfs.Claims{}, func(t *jwt.Token) (any, error) {
+		k := setting.LFS.SigningKey
+		if t.Method != k.SigningMethod() {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return k.VerifyKey(), nil
+	})
+	if err != nil {
+		return &auth.AuthenticationAttemptedIncorrectCredential{Error: err}
+	}
+
+	claims, claimsOk := token.Claims.(*lfs.Claims)
+	if !token.Valid {
+		return &auth.AuthenticationAttemptedIncorrectCredential{Error: errors.New("not a valid LFS JWT")}
+	} else if !claimsOk {
+		return &auth.AuthenticationError{Error: fmt.Errorf("claim object %v was not an lfs.Claims instance", token.Claims)}
+	}
+
+	u, err := user_model.GetUserByID(req.Context(), claims.UserID)
+	if err != nil {
+		return &auth.AuthenticationError{Error: fmt.Errorf("unable to load claim user %d: %w", claims.UserID, err)}
+	}
+	if !u.IsAccessAllowed(req.Context()) {
+		return &auth.AuthenticationError{Error: fmt.Errorf("user access is not permitted")}
+	}
+
+	return &auth.AuthenticationSuccess{
+		Result: &lfsTokenAuthenticationResult{
+			user:   u,
+			claims: claims,
+		},
+	}
+}

services/auth/method/lfs_token_test.go
@@ -1,0 +1,160 @@
+// Copyright 2025 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package method
+
+import (
+	"fmt"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	auth_model "forgejo.org/models/auth"
+	"forgejo.org/models/unittest"
+	"forgejo.org/modules/lfs"
+	"forgejo.org/modules/optional"
+	"forgejo.org/modules/setting"
+	"forgejo.org/services/auth"
+	"forgejo.org/services/authz"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type authTokenOptions struct {
+	Op     string
+	UserID int64
+	RepoID int64
+}
+
+func getLFSAuthTokenWithBearer(opts authTokenOptions) (string, error) {
+	now := time.Now()
+	claims := lfs.Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(now.Add(setting.LFS.HTTPAuthExpiry)),
+			NotBefore: jwt.NewNumericDate(now),
+		},
+		RepoID: opts.RepoID,
+		Op:     opts.Op,
+		UserID: opts.UserID,
+	}
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := setting.LFS.SigningKey.JWT(claims)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign LFS JWT token: %w", err)
+	}
+	return "Bearer " + tokenString, nil
+}
+
+func testAuthenticate(t *testing.T, cfg string) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+	var err error
+	setting.CfgProvider, err = setting.NewConfigProviderFromData(cfg)
+	require.NoError(t, err, "Config")
+	setting.LoadCommonSettings()
+
+	t.Run("no bearer token", func(t *testing.T) {
+		a := &LFSToken{}
+		req := httptest.NewRequest("GET", "https://example.org", nil)
+		output := a.Verify(req, nil, nil)
+		requireOutput[*auth.AuthenticationNotAttempted](t, output)
+	})
+
+	t.Run("bearer not a JWT", func(t *testing.T) {
+		a := &LFSToken{}
+		req := httptest.NewRequest("GET", "https://example.org", nil)
+		req.Header.Set("Authorization", "Bearer abc")
+		output := a.Verify(req, nil, nil)
+		err := requireOutput[*auth.AuthenticationAttemptedIncorrectCredential](t, output).Error
+		require.ErrorContains(t, err, "token is malformed")
+	})
+
+	t.Run("token valid op=download", func(t *testing.T) {
+		bearerAuth, err := getLFSAuthTokenWithBearer(authTokenOptions{Op: "download", UserID: 2, RepoID: 1})
+		require.NoError(t, err)
+		a := &LFSToken{}
+		req := httptest.NewRequest("GET", "https://example.org", nil)
+		req.Header.Set("Authorization", bearerAuth)
+		output := a.Verify(req, nil, nil)
+		result := requireOutput[*auth.AuthenticationSuccess](t, output).Result
+		assert.EqualValues(t, 2, result.User().ID)
+		assert.Equal(t, optional.Some(auth_model.AccessTokenScopeReadRepository), result.Scope())
+		require.NotNil(t, result.Reducer())
+
+		// No direct way to query an authz.Reducer for its specific repos permitted, so this is a bit of a workaround to
+		// see if it's targeting the repo from the JWT:
+		repoGetter, isRepoGetter := result.(authz.RepoGetter)
+		require.True(t, isRepoGetter)
+		assert.EqualValues(t, 1, repoGetter.GetTargetRepoID())
+	})
+
+	t.Run("token valid op=upload", func(t *testing.T) {
+		bearerAuth, err := getLFSAuthTokenWithBearer(authTokenOptions{Op: "upload", UserID: 2, RepoID: 24})
+		require.NoError(t, err)
+		a := &LFSToken{}
+		req := httptest.NewRequest("GET", "https://example.org", nil)
+		req.Header.Set("Authorization", bearerAuth)
+		output := a.Verify(req, nil, nil)
+		result := requireOutput[*auth.AuthenticationSuccess](t, output).Result
+		assert.EqualValues(t, 2, result.User().ID)
+		assert.Equal(t, optional.Some(auth_model.AccessTokenScopeWriteRepository), result.Scope())
+		require.NotNil(t, result.Reducer())
+
+		// No direct way to query an authz.Reducer for its specific repos permitted, so this is a bit of a workaround to
+		// see if it's targeting the repo from the JWT:
+		repoGetter, isRepoGetter := result.(authz.RepoGetter)
+		require.True(t, isRepoGetter)
+		assert.EqualValues(t, 24, repoGetter.GetTargetRepoID())
+	})
+
+	t.Run("token signature malformed", func(t *testing.T) {
+		bearerAuth, err := getLFSAuthTokenWithBearer(authTokenOptions{Op: "download", UserID: 2, RepoID: 1})
+		bearerAuth += "malformed"
+
+		require.NoError(t, err)
+		a := &LFSToken{}
+		req := httptest.NewRequest("GET", "https://example.org", nil)
+		req.Header.Set("Authorization", bearerAuth)
+		output := a.Verify(req, nil, nil)
+		err = requireOutput[*auth.AuthenticationAttemptedIncorrectCredential](t, output).Error
+		require.ErrorContains(t, err, "token signature is invalid")
+	})
+
+	t.Run("invalid user", func(t *testing.T) {
+		bearerAuth, err := getLFSAuthTokenWithBearer(authTokenOptions{Op: "download", UserID: 999, RepoID: 1})
+		require.NoError(t, err)
+		a := &LFSToken{}
+		req := httptest.NewRequest("GET", "https://example.org", nil)
+		req.Header.Set("Authorization", bearerAuth)
+		output := a.Verify(req, nil, nil)
+		err = requireOutput[*auth.AuthenticationError](t, output).Error
+		require.ErrorContains(t, err, "user does not exist")
+	})
+}
+
+type namedCfg struct {
+	name, cfg string
+}
+
+var iniCommon = `[security]
+INSTALL_LOCK = true
+INTERNAL_TOKEN = ForgejoForgejoForgejoForgejoForgejoForgejo_	# don't use in prod
+[oauth2]
+JWT_SECRET = ForgejoForgejoForgejoForgejoForgejoForgejo_	# don't use in prod
+[server]
+LFS_START_SERVER = true
+	`
+
+var cfgVariants = []namedCfg{
+	{name: "HS256_default", cfg: `LFS_JWT_SECRET = ForgejoForgejoForgejoForgejoForgejoForgejo_`},
+	{name: "RS256", cfg: `LFS_JWT_SIGNING_ALGORITHM = RS256`},
+}
+
+func TestAuthenticate(t *testing.T) {
+	for _, v := range cfgVariants {
+		cfg := iniCommon + v.cfg
+		t.Run(v.name, func(t *testing.T) { testAuthenticate(t, cfg) })
+	}
+}

services/authz/access_token.go
@@ -30,7 +30,7 @@ func GetAuthorizationReducerForAccessToken(ctx context.Context, token *auth_mode
 	for i, r := range repos {
 		iface[i] = r
 	}
-	return &SpecificReposAuthorizationReducer{resourceRepos: iface}, nil
+	return &SpecificReposAuthorizationReducer{ResourceRepos: iface}, nil
 }
 
 // Validate that an access token's state is valid for creation.  For example, that it doesn't have a conflicting set of

services/authz/access_token_test.go
@@ -41,8 +41,8 @@ func TestGetAuthorizationReducerForAccessToken(t *testing.T) {
 		require.True(t, ok)
 		require.NotNil(t, specific)
 
-		require.Len(t, specific.resourceRepos, 1)
-		assert.EqualValues(t, 1, specific.resourceRepos[0].GetTargetRepoID())
+		require.Len(t, specific.ResourceRepos, 1)
+		assert.EqualValues(t, 1, specific.ResourceRepos[0].GetTargetRepoID())
 	})
 }
 

commit e2dc0d8996571e35d51b9ff102d38b1ef6cede5a

fix: enable reverse-proxy authentication on git routes (#13640)
Fixes #13627, bug introduced by #12725.  Similar to #13519, a valid authentication method was missed when splitting up the authentication methods to be applied to each route.

## Testing

Manual tested:

- set `[service].ENABLE_REVERSE_PROXY_AUTHENTICATION = true`
- set `[server].PROTOCOL = http+unix`
- set `[server].HTTP_ADDR = /home/mfenniak/Dev/forgejo.sock`
- attempt to access git information on a private repository (`mfenniak/test`):
    - `curl -v -H "X-WebAuth-User: mfenniak" --unix-socket ///home/mfenniak/Dev/forgejo.sock localhost/mfenniak/test/info/refs`
    - confirm failed with 401
- apply patch
- reattempt accessing git information; confirm `200 OK` with a list of git references on the private repository
- mutate `X-WebAuth-User:` to a non-existent user, confirmed 401 error

Similar to #13587, an automated test for this situation seems impractical -- `[service].ENABLE_REVERSE_PROXY_AUTHENTICATION` cannot be changed during the tests because it is read once during the routing configuration, which is very early in the `TestMain` process setup, is reused in all the integration tests, and cannot be changed during the test.

Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/13640
Reviewed-by: Gusted <gusted@noreply.codeberg.org>

routers/web/web.go
@@ -162,6 +162,12 @@ func buildGitLfsAuthGroup() *auth_method.Group {
 		// is enabled for Authorized Integrations as well:
 		PermitBasic: true,
 	})
+	if setting.Service.EnableReverseProxyAuth {
+		// reverseproxy should before Session, otherwise the header will be ignored if user has login
+		group.Add(&auth_method.ReverseProxy{
+			CreateSession: true,
+		})
+	}
 	return group
 }
 
@@ -185,6 +191,12 @@ func buildGitAuthGroup() *auth_method.Group {
 		// is enabled for Authorized Integrations as well:
 		PermitBasic: true,
 	})
+	if setting.Service.EnableReverseProxyAuth {
+		// reverseproxy should before Session, otherwise the header will be ignored if user has login
+		group.Add(&auth_method.ReverseProxy{
+			CreateSession: true,
+		})
+	}
 	return group
 }
 