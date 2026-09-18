0aa63c182106086e690764248433d68b034b4ceb
0ko <0ko@noreply.codeberg.org>
2026-09-18 09:19:30 +0200

    feat(ui): add focus outline to buttons (#14281)
    Rel:
    
    https://codeberg.org/forgejo/forgejo/issues/11894
    https://codeberg.org/forgejo/forgejo/pulls/14161
    https://codeberg.org/forgejo/forgejo/pulls/14266
    
    Changes:
    
    * refactor CSS so that specific style rules are setting scoped variables rather than properties, and so that only the main rule sets all properties (color, background, border, outline). This is a cleaner way of doing this but functionally same
    * add 3px semi-transparent outlines appearing when buttons are in `:focus-visible` state, e.g. they were navigated to by keyboard
    * add border to `primary` style. `border` actually has an effect on layout, but since the buttons are limited by max-height, this wasn't well visible. However, non-primary were 2px wider than primary for same contents. I also think a slight border would make primary buttons look more distinctly as buttons, not just round rectangles
    * in demo, fixed text in `secondary` button example
    
    Preview:
    
    Before D: https://codeberg.org/attachments/f469265b-1849-4f61-82a1-d3a8cfcaa067
    Before L: https://codeberg.org/attachments/35a7885b-11d5-4eec-b8dd-ce102710447d
    
    After 1D: https://codeberg.org/attachments/34cb7ec0-7952-44fb-b16a-d343bff358e3
    After 1L: https://codeberg.org/attachments/6a294b0e-2c8d-40f9-8b63-4c6cf9fc64a5
    
    After 2D: https://codeberg.org/attachments/27c2e107-511e-4cae-b564-7ba6758993dc
    After 2L: https://codeberg.org/attachments/f689066b-d6da-4ecb-a893-1458e8504543
    
    Multiple buttons having `:focus-visible` is simulated in-code to save on screenshots.
    
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/14281
    Reviewed-by: AverageHelper <averagehelper@noreply.codeberg.org>

--- tests/e2e/buttons.test.e2e.ts
+++ tests/e2e/buttons.test.e2e.ts
@@ -43,6 +43,7 @@ test('Button visuals', async ({browser}) => {
         fontWeight: s.fontWeight,
         opacity: s.opacity,
         pointerEvents: s.pointerEvents,
+        borderWidth: s.borderWidth,
       };
     });
   }
@@ -65,6 +66,8 @@ test('Button visuals', async ({browser}) => {
     expect(item.fontWeight).toBe('500');
     // Evaluate opacity
     expect(item.opacity).toBe('1');
+    // Evaluate border width
+    expect(item.borderWidth).toBe('1px');
   }
 
   // Evaluate that background-colors are different
@@ -75,6 +78,7 @@ test('Button visuals', async ({browser}) => {
   const secondaryDisabled = await getButtonProperties(page, '.button.secondary.disabled');
   const dangerDisabled = await getButtonProperties(page, '.button.danger.disabled');
 
+  // Evaluate properties specific to disabled buttons
   for (const item of [primaryDisabled, secondaryDisabled, dangerDisabled]) {
     // Evaluate opacity
     expect(item.opacity).toBe('0.55');
@@ -84,5 +88,15 @@ test('Button visuals', async ({browser}) => {
     // Evaluate other properties of non-disabled buttons
     expect(item.backgroundColor).not.toBe(transparent);
     expect(item.fontWeight).toBe('500');
+  }
+
+  // Evaluate outline from `:focus-visible` appearing
+  for (const button of await page.locator('.page-content .button-sequence .button').all()) {
+    // First, get to the element with .focus(), then activate :focus-visible by keyboard
+    await button.focus();
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Shift+Tab');
+
+    expect(await button.evaluate((el) => getComputedStyle(el).outlineWidth)).toBe('3px');
   }
 });
