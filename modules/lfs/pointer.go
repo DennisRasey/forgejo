2a126b4c1b017cbea100fb8f4bc0b1ce491c6293
Gusted <gusted@noreply.codeberg.org>
2026-09-25 22:04:32 +0200

    revert 089a185c72af8fff0c950deee9bad97aaf61742b
    Got reported a possible panic related to this change, back it out for v17 branch.

--- modules/lfs/pointer.go
+++ modules/lfs/pointer.go
@@ -16,10 +16,7 @@ import (
 )
 
 const (
-	// LFS pointer files must be less than 1024 bytes
-	//
-	// See: https://github.com/git-lfs/git-lfs/blob/f0bffc4fe998fe5cb004dbca9e8951ea662ff66b/docs/spec.md?plain=1#L22-L23
-	BlobSizeCutoff = 1024
+	blobSizeCutoff = 1024
 
 	// MetaFileIdentifier is the string appearing at the first line of LFS pointer files.
 	// https://github.com/git-lfs/git-lfs/blob/master/docs/spec.md
@@ -42,7 +39,7 @@ var (
 
 // ReadPointer tries to read LFS pointer data from the reader
 func ReadPointer(reader io.Reader) (Pointer, error) {
-	buf := make([]byte, BlobSizeCutoff)
+	buf := make([]byte, blobSizeCutoff)
 	n, err := io.ReadFull(reader, buf)
 	if err != nil && err != io.ErrUnexpectedEOF {
 		return Pointer{}, err
--- routers/web/repo/setting/lfs.go
+++ routers/web/repo/setting/lfs.go
@@ -271,7 +271,7 @@ func LFSFileGet(ctx *context.Context) {
 		return
 	}
 	defer dataRc.Close()
-	buf := make([]byte, lfs.BlobSizeCutoff)
+	buf := make([]byte, 1024)
 	n, err := util.ReadAtMost(dataRc, buf)
 	if err != nil {
 		ctx.ServerError("LFSFileGet", err)
